9bd979934a2ffaa7e6ab01f99b21940654b008a5
Justin Frank <justinpfrank@protonmail.com>
2021-12-10 18:52:04 -0500

    Refactor stack lang to have a cap on stack height

--- StackLang.v
+++ StackLang.v
@@ -85,46 +85,41 @@ Inductive result : Type :=
   | Value (v : ins_val).
 
 Inductive stk_ins : Type :=
+  | Err
   | Call (l : string) (n : nat)
-  | Ret
   | Push (v : ins_val)
   | Pop
   | StkRef (n : nat)
   | Uop (u : ins_uop)
   | Bop (b : ins_bop)
-  | Cmp (c : ins_cmp)
-  | If (thn : list stk_ins) (els : list stk_ins).
+  | Cmp (c : ins_cmp).
+
+Inductive stk_tm : Type :=
+  | End
+  | Ins (ins : stk_ins) (rst : stk_tm)
+  | If (thn : stk_tm) (els : stk_tm) (rst : stk_tm).
 
 Inductive stk_fun : Type :=
-  Fun (l : string) (n : nat) (ins : list stk_ins).
+  Fun (l : string) (n : nat) (ins : stk_tm).
 
 Inductive stk_prg : Type :=
-  Prg (funs : list stk_fun) (inss : list stk_ins).
+  Prg (funs : list stk_fun) (inss : stk_tm).
 
-Inductive stk_stk : Type :=
-  | MtStk
-  | Frame (inss : list stk_ins) (vals : list ins_val) (rst : stk_stk).
-
-Definition eval_ins :=
+Definition eval_tm :=
   fun (funs : partial_map stk_fun) =>
   fix eval' (f : nat) :=
-  let oof_check :=
-    match f with
-    | O => fun _ _ _ => OOF
-    | S f' => eval' f'
-    end in
-  fix eval'' (call_stack : stk_stk) :=
-  fix eval''' (inss : list stk_ins) :=
+  fix eval''' (inss : stk_tm) :=
   fun (val_stack : list ins_val) =>
 
   match inss with
-  | nil =>
+  | End =>
     match val_stack with
     | nil => Error
     | v :: _ => Value v
     end
-  | ins :: inss' =>
+  | Ins ins inss' =>
     match ins with
+    | Err => Error
     | Call l n =>
       let args := firstn n val_stack in
       let rst := skipn n val_stack in
@@ -135,19 +130,18 @@ Definition eval_ins :=
         | None => Error
         | Some (Fun _ m inss'') =>
           if m =? n then
-            oof_check (Frame inss' rst call_stack) inss'' args
+            match f with
+            | O => OOF
+            | S f' =>
+              match eval' f' inss'' args with
+              | OOF => OOF
+              | Error => Error
+              | Value v => eval''' inss' rst
+              end
+            end
           else
             Error
         end
-    | Ret =>
-      match call_stack with
-      | MtStk => Error
-      | Frame inss'' vals rst =>
-        match val_stack with
-        | nil => Error
-        | v :: _ => eval'' rst inss'' (v :: vals)
-        end
-      end
     | Push v => eval''' inss' (v :: val_stack)
     | Pop =>
       match val_stack with
@@ -182,12 +176,22 @@ Definition eval_ins :=
       | V_Int i1 :: V_Int i2 :: rst => eval''' inss' (V_Bool (do_cmp c i1 i2) :: rst)
       | _ => Error
       end
-    | If thn els =>
-      match val_stack with
-      | V_Bool true :: rst => oof_check call_stack (thn ++ inss') rst
-      | V_Bool false :: rst => oof_check call_stack (els ++ inss') rst
-      | _ => Error
+    end
+  | If thn els nxt =>
+    match val_stack with
+    | V_Bool false :: rst =>
+      match eval''' els rst with
+      | OOF => OOF
+      | Error => Error
+      | Value v => eval''' nxt (v :: rst)
       end
+    | _ :: rst =>
+      match eval''' thn rst with
+      | OOF => OOF
+      | Error => Error
+      | Value v => eval''' nxt (v :: rst)
+      end
+    | _ => Error
     end
   end.
 
@@ -199,7 +203,7 @@ Fixpoint extract_funs (funs : list stk_fun) :=
 
 Definition eval (f : nat) (prg : stk_prg) :=
   match prg with
-  | Prg funs inss => eval_ins (extract_funs funs) f MtStk inss nil
+  | Prg funs inss => eval_tm (extract_funs funs) f inss nil
   end.
 
 End StackLang.
